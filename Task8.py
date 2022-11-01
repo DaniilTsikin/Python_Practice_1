3f2ee6e5ad2670b07ad13d88c31ad2d265cc46ed
Daniil <dtsikin@mail.ru>
2022-11-01 22:25:30 +0500

    fixed task8

--- Task8.py
+++ Task8.py
@@ -1,9 +1,19 @@
 #Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 # Пример: - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-n = int(input('Введите число: '))
-print(*[((1 + 1 / m) ** m) for m in range(1, n + 1)])
-print(f'Сумма чисел последовательности: {(round(sum(list(map(lambda m: ((1 + 1 / m) ** m), range(1, n + 1)))), 3))}')
+def FindProduct(list1, list2):
+    product = 1
+    for i in range(len(list2)):
+        product *= list1[list2[i]]
+    return product
+
+N = int(input('Введите число N: '))
+
+first_list = list(range(-N, N+1))
+second_list = list(range(5))
+
+for i in range(len(second_list)):
+    second_list[i] = int(input("Введите номер индекса: "))
 
 print(first_list)
 print(second_list)
